bbe3bf9b5cfa6a21060f26182fe5df84d2b5399b
Mitra Bavakhani <m_bavakhani@csdiran.ir>
2023-08-21 15:22:59 +0330

    Upload Design Patterns - Singleton project on Github

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using DesignPatterns.Singleton;
+
+namespace DesignPatterns
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Singleton
+            
+            MySingleton instance1 = MySingleton.GetInstance();
+            MySingleton instance2 = MySingleton.GetInstance();
+            MySingleton instance3 = MySingleton.GetInstance();
+
+            #endregion Singleton
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+
+
+    public class MySingleton
+    {
+        private static MySingleton _instance = null;
+        private static readonly object padlock = new object();
+        private static int Counter = 0;
+
+        private MySingleton()
+        {
+            Counter++;
+            Console.WriteLine("Counter Vlue" + Counter.ToString());
+
+        }
+
+        public static MySingleton GetInstance()
+        {
+
+            lock (padlock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new MySingleton();
+                }
+
+                return _instance;
+            }
+        }
+
+    }
+}
